ee2b8953ee64a14d8ceef735af0a66a3bfab6ad6
Alex Karadimos <karadalex@gmail.com>
2018-04-15 17:58:32 +0300

    Check if Blockchain is static - TODO: write test

--- src/main/java/com/alexkaradimos/blockchain/BlockchainApplication.java
+++ src/main/java/com/alexkaradimos/blockchain/BlockchainApplication.java
@@ -1,5 +1,7 @@
 package com.alexkaradimos.blockchain;
 
+import com.alexkaradimos.blockchain.models.Block;
+import com.alexkaradimos.blockchain.models.Blockchain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlockchainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlockchainApplication.class, args);
+		Blockchain springChain1 = new Blockchain();
+		System.out.println(Blockchain.getLatestBlock().toString());
+		int index = Blockchain.getLatestBlock().getIndex();
+		springChain1.addBlock(new Block(++index, "time", "prevHash", "data"));
+		System.out.println(Blockchain.getLatestBlock().toString());
+
+		Blockchain springChain2 = new Blockchain();
+		System.out.println(springChain2.getLatestBlock().toString());
+		springChain2.addBlock(new Block(++index, "time", "prevHash", "data"));
+		System.out.println(Blockchain.getLatestBlock().toString());
+
+		Blockchain springChain3 = new Blockchain();
+		springChain3.addBlock(new Block(++index, "time", "prevHash", "data"));
+		System.out.println(springChain3.getLatestBlock().toString());
+
+
+//		SpringApplication.run(BlockchainApplication.class, args);
 	}
 }
