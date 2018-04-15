357b887b69098cd1332915fee64752ab42ea4502
Alex Karadimos <karadalex@gmail.com>
2018-04-15 21:58:18 +0300

    Just initialize Blockchain once and interact with it  through the API endpoints

--- src/main/java/com/alexkaradimos/blockchain/BlockchainApplication.java
+++ src/main/java/com/alexkaradimos/blockchain/BlockchainApplication.java
@@ -1,6 +1,5 @@
 package com.alexkaradimos.blockchain;
 
-import com.alexkaradimos.blockchain.models.Block;
 import com.alexkaradimos.blockchain.models.Blockchain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,19 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlockchainApplication {
 
 	public static void main(String[] args) {
-		Blockchain springChain1 = new Blockchain();
-		System.out.println(Blockchain.getLatestBlock().toString());
-		springChain1.addBlock("data1");
-
-		Blockchain springChain2 = new Blockchain();
-		System.out.println(springChain2.getLatestBlock().toString());
-		springChain2.addBlock("data2");
-		System.out.println(Blockchain.getLatestBlock().toString());
-
-		Blockchain springChain3 = new Blockchain();
-		springChain3.addBlock("data3");
-		System.out.println(springChain3.getLatestBlock().toString());
-
+		new Blockchain();
 
 		SpringApplication.run(BlockchainApplication.class, args);
 	}
