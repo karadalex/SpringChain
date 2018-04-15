fd79d4fe3164dba3cd949355ea9aa4f69df62023
Alex Karadimos <karadalex@gmail.com>
2018-04-15 17:57:47 +0300

    Initialize new Blockchain only if null (only once in runtime) and create GenesisBlock

--- src/main/java/com/alexkaradimos/blockchain/models/Blockchain.java
+++ src/main/java/com/alexkaradimos/blockchain/models/Blockchain.java
@@ -1,19 +1,35 @@
 package com.alexkaradimos.blockchain.models;
 
+import java.util.ArrayList;
+
 /**
  * Created by karadalex on 14/4/2018.
  */
 public class Blockchain {
 
-    public void createGenesisBlock() {
+    static ArrayList<Block> blocks;
 
+    public Blockchain() {
+        if (blocks == null) {
+            blocks = new ArrayList<Block>();
+            createGenesisBlock();
+        }
     }
 
-    public Block getLatestBlock() {
-        return new Block(1, "time", "prevhash", "data");
+    private void createGenesisBlock() {
+        blocks.add(new Block(0, "time", "0", "GenesisBlock"));
+    }
+
+    public static ArrayList<Block> getBlocks() {
+        return blocks;
+    }
+
+    public static Block getLatestBlock() {
+        return blocks.get(blocks.size()-1);
     }
 
     public void addBlock(Block newBlock) {
+        blocks.add(newBlock);
     }
 
     public Boolean isChainValid() {
