d8729439c37f0be6d4d2df78dc5f833c30214a85
Alex Karadimos <karadalex@gmail.com>
2018-04-17 01:14:18 +0300

    1. Initialize with difficulty 2. Create UUID 3. Store subBlocks at the Blockchain Blocks

--- src/main/java/com/alexkaradimos/blockchain/models/SubBlockchain.java
+++ src/main/java/com/alexkaradimos/blockchain/models/SubBlockchain.java
@@ -1,6 +1,7 @@
 package com.alexkaradimos.blockchain.models;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  * Created by karadalex on 16/4/2018.
@@ -8,19 +9,25 @@ import java.util.ArrayList;
 public class SubBlockchain {
 
     public ArrayList<SubBlock> subBlocks;
-    static int latestIndex;
-    static int difficulty = 5;
+    private String subBlockchainId;
+    public int latestIndex;
+    public int difficulty;
 
-    public SubBlockchain() {
+    public SubBlockchain(int difficulty) {
         if (subBlocks == null) {
             subBlocks = new ArrayList<SubBlock>();
             createGenesisBlock();
             latestIndex = 0;
         }
+        this.subBlockchainId = UUID.randomUUID().toString();
+        this.difficulty = difficulty;
     }
 
     private void createGenesisBlock() {
-        subBlocks.add(new SubBlock(0, "0", "GenesisSubBlock"));
+        SubBlock genesis = new SubBlock(0, "0", "GenesisSubBlock");
+        subBlocks.add(genesis);
+        // TODO: find a way to reduce duplicate subBlock
+        Blockchain.getLatestBlock().addSubBlock(genesis);
     }
 
     public ArrayList<SubBlock> getSubBlocks() {
@@ -31,11 +38,15 @@ public class SubBlockchain {
         return subBlocks.get(latestIndex);
     }
 
-    public static int getLatestIndex() {
+    public String getSubBlockchainId() {
+        return subBlockchainId;
+    }
+
+    public int getLatestIndex() {
         return latestIndex;
     }
 
-    public static int getDifficulty() {
+    public int getDifficulty() {
         return difficulty;
     }
 
@@ -43,6 +54,8 @@ public class SubBlockchain {
         String prevHash = getLatestSubBlock().getHash();
         SubBlock newBlock = new SubBlock(++latestIndex, prevHash, data);
         subBlocks.add(newBlock);
+        // TODO: find a way to reduce duplicate subBlock
+        Blockchain.getLatestBlock().addSubBlock(newBlock);
         return newBlock;
     }
 
@@ -55,4 +68,13 @@ public class SubBlockchain {
         return true;
     }
 
+    @Override
+    public String toString() {
+        return "SubBlockchain{" +
+                "subBlocks=" + subBlocks +
+                ", subBlockchainId='" + subBlockchainId + '\'' +
+                ", latestIndex=" + latestIndex +
+                ", difficulty=" + difficulty +
+                '}';
+    }
 }
