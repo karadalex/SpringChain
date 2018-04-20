157ef11884c7f4b7d571ba3b6a922ed1b4531119
Alex Karadimos <karadalex@gmail.com>
2018-04-20 18:40:00 +0300

    TODO: use Kafka as the consensus mechanism

---
+++ src/main/java/com/alexkaradimos/blockchain/models/KafkaSpringChainMessage.java
@@ -0,0 +1,7 @@
+package com.alexkaradimos.blockchain.models;
+
+/**
+ * Created by karadalex on 19/4/2018.
+ */
+public class KafkaSpringChainMessage {
+}
---
+++ src/main/java/com/alexkaradimos/blockchain/services/KafkaSpringChainProducer.java
@@ -0,0 +1,7 @@
+package com.alexkaradimos.blockchain.services;
+
+/**
+ * Created by karadalex on 19/4/2018.
+ */
+public class KafkaSpringChainProducer {
+}
